0d231ff14ff9c455c35a3fc27b884756c7ab91e0
Yuri Caetano <yuuri.caetano@gmail.com>
2023-07-24 18:21:10 -0300

    update route

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.IP_SERVER = "https://apiwpp.brconnect.click/api/";
-window.IP_SOCKET_IO = "https://apiwpp.brconnect.click/";
+window.IP_SERVER = "https://wppconnect.brconnect.click/api/";
+window.IP_SOCKET_IO = "https://wppconnect.brconnect.click/";
